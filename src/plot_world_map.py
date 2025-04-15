fac3d7eb507079908de7c3679f75a5defb94bb56
davidedomini <davide.domini@unibo.it>
2025-04-15 12:05:17 +0200

    feat: implement world plotting

--- src/plot_world_map.py
+++ src/plot_world_map.py
@@ -6,18 +6,30 @@ import matplotlib.pyplot as plt
 
 def get_data(data_path):
     files = glob.glob(f'{data_path}/*.csv')
-    data = {}
+    data = {
+        'country': [],
+        'Carbon intensity gCO₂eq/kWh (direct)': []
+    }
     for file in files:
         df = pd.read_csv(file)
-        print(df)
         country = df['Country'].iloc[0]
-        value = df['Carbon intensity gCO₂eq/kWh (direct)'].mean()
-        data[country] = value
-    return data
+        if 'Bosnia' in country:
+            country = 'Bosnia and Herz.'
+        data['country'].append(country)
+        data['Carbon intensity gCO₂eq/kWh (direct)'].append(df['Carbon intensity gCO₂eq/kWh (direct)'].mean())
+    return pd.DataFrame(data)
 
 def plot_map(data):
-   pass
-    
+    world = gpd.read_file('data/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp')
+    print(world['NAME'].sort_values().unique())
+    merged = world.merge(data, how='left', left_on='NAME', right_on='country')
+    merged = merged[merged['Carbon intensity gCO₂eq/kWh (direct)'].notnull()]
+    fig, ax = plt.subplots(1, 1, figsize=(10, 6))
+    world.boundary.plot(ax=ax, linewidth=0.5, color='gray')  # bordi del mondo
+    merged.plot(column='Carbon intensity gCO₂eq/kWh (direct)', ax=ax, legend=True, cmap='RdYlGn_r', edgecolor='black')
+    ax.set_xlim([-15, 30])  # longitudes
+    ax.set_ylim([35, 60])   # latitude
+    plt.savefig('world_map.pdf', dpi=300)
 
 if __name__ == '__main__':
 
